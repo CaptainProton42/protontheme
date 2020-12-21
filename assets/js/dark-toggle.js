12c54b01d814ce40fd571d336b09ee9a2dc843e1
John Wigg <31868812+CaptainProton42@users.noreply.github.com>
2020-12-21 19:46:56 +0100

    Update dark-toggle.js

--- assets/js/dark-toggle.js
+++ assets/js/dark-toggle.js
@@ -13,5 +13,5 @@ function onpageshow() {
 function toggle_dark_mode() {
     localStorage.getItem('mode') === 'dark' ? document.querySelector('body').classList.add('dark') : document.querySelector('body').classList.remove('dark');
     var toggle = document.getElementById('dark-toggle');
-    localStorage.getItem('mode') === 'dark' ? toggle.innerHTML = '<i class="material-icons left">brightness_high</i>' : toggle.innerHTML = '<i class="material-icons left">brightness_low</i>';
+    localStorage.getItem('mode') === 'dark' ? toggle.innerHTML = '<i class="material-icons left">brightness_high</i>' : toggle.innerHTML = '<i class="material-icons left">brightness_4</i>';
 }
